0eb326c4c26fb3a7fee6e58d6a31b7295b23b507
Mark McDaniels <mcdaniels.mark@gmail.com>
2018-10-19 23:31:12 -0400

    Work for 10/19/18
    Info for individual challenges in readme.

---
+++ react-redux/mapDynRender.js
@@ -0,0 +1,53 @@
+const textAreaStyles = {
+  width: 235,
+  margin: 5
+};
+
+class MyToDoList extends React.Component {
+  constructor(props) {
+    super(props);
+    // change code below this line
+      this.state ={
+        userInput:"",
+        toDoList: []
+      };
+    // change code above this line
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleSubmit() {
+    const itemsArray = this.state.userInput.split(',');
+    this.setState({
+      toDoList: itemsArray
+    });
+  }
+  handleChange(e) {
+    this.setState({
+      userInput: e.target.value
+    });
+  }
+  render() {
+    const items = this.state.toDoList.map(function(currentValue){
+      // syntax seems inconsistent
+      // need to remember that this is
+      // inside render, html is unquoted
+      // and variables are in curlies
+      return <li>{currentValue}</li> ;
+    });
+    
+    return (
+      <div>
+        <textarea
+          onChange={this.handleChange}
+          value={this.state.userInput}
+          style={textAreaStyles}
+          placeholder="Separate Items With Commas" /><br />
+        <button onClick={this.handleSubmit}>Create List</button>
+        <h1>My "To Do" List:</h1>
+        <ul>
+          {items}
+        </ul>
+      </div>
+    );
+  }
+};
---
+++ react-redux/uniqueKeyAtt.js
@@ -0,0 +1,25 @@
+
+const frontEndFrameworks = [
+  'React',
+  'Angular',
+  'Ember',
+  'Knockout',
+  'Backbone',
+  'Vue'
+];
+
+function Frameworks() {
+  const renderFrameworks = frontEndFrameworks.map(function(currentValue){
+    let str = <li key={currentValue}>{currentValue}</li>;
+    return str;
+   
+  }); // change code here
+  return (
+    <div>
+      <h1>Popular Front End JavaScript Frameworks</h1>
+      <ul>
+        {renderFrameworks}
+      </ul>
+    </div>
+  );
+};
